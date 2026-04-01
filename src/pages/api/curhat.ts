c95f4577ff2b0e973fbcd33406585c856c372045
rasyaradityaputra25 <rasyaradityaputra25@gmail.com>
2026-04-01 15:25:19 +0000

    page pilar

--- src/pages/api/curhat.ts
+++ src/pages/api/curhat.ts
@@ -1,4 +1,6 @@
 import type { APIRoute } from 'astro';
+import { db } from '../../firebase/config';
+import { collection, addDoc } from "firebase/firestore"; 
 
 export const post: APIRoute = async ({ request }) => {
   const data = await request.formData();
@@ -6,22 +8,52 @@ export const post: APIRoute = async ({ request }) => {
   const question1 = data.get('question1');
   const question2 = data.get('question2');
 
-  // For this example, we'll just log it to the console.
-  console.log({
-    initials,
-    question1,
-    question2,
-  });
+  if (!initials || !question1 || !question2) {
+    return new Response(
+      JSON.stringify({
+        message: 'Semua kolom wajib diisi.',
+      }),
+      {
+        status: 400,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  }
 
-  return new Response(
-    JSON.stringify({
-      message: 'Curhat berhasil dikirim!',
-    }),
-    {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+  try {
+    const docRef = await addDoc(collection(db, "curhatan"), {
+      initials: initials,
+      question1: question1,
+      question2: question2,
+      timestamp: new Date()
+    });
+    console.log("Document written with ID: ", docRef.id);
+
+    return new Response(
+      JSON.stringify({
+        message: 'Curhat berhasil dikirim!',
+      }),
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  } catch (e) {
+    console.error("Error adding document: ", e);
+    return new Response(
+      JSON.stringify({
+        message: 'Gagal mengirim curhat.',
+      }),
+      {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  }
 };
