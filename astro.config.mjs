ebb5497700ad5959b32d5ecd8f935af0cb4f5ceb
rasyaradityaputra25 <rasyaradityaputra25@gmail.com>
2026-04-03 16:39:36 +0000

    1.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
-    adapter: vercel()
+    adapter: vercel({
+        analytics: true
+    })
 });
